31bb98a92511f057a0c5d2cf9ba17ce48f1c9d0a
Steve Crow <steve@smcrow.com>
2025-04-23 22:06:37 -0400

    Remove container in header and footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -64,7 +64,7 @@
 
 <div class="flex h-[100dvh] flex-col">
 	<nav class="bg-gradient-to-l from-zinc-800/50 to-purple-950/50 text-white">
-		<div class="container mx-auto flex flex-grow items-center justify-between px-4 py-2">
+		<div class="flex flex-grow items-center justify-between px-4 py-2">
 			<!-- Logo and Create Button -->
 			<div class="flex items-center gap-2">
 				<!-- Logo -->
@@ -212,7 +212,7 @@
 	</div>
 
 	<footer class="mt-auto bg-gradient-to-l from-zinc-800/50 to-purple-950/50 text-white">
-		<div class="container mx-auto flex flex-col gap-4 px-4 py-2">
+		<div class="flex flex-col gap-4 px-4 py-2">
 			<div class="flex items-center justify-between text-sm">
 				<div class="flex items-center">
 					© 2025 <Logo class="mr-1 ml-2 h-4 w-4" /> <span class="font-bold">Retro</span><span
