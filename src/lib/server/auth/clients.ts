c458d08eb7cd712e38d1428c0a8712a951fd1d9a
Steve Crow <steve@smcrow.com>
2025-05-02 14:18:35 -0400

    Fix build

--- src/lib/server/auth/clients.ts
+++ src/lib/server/auth/clients.ts
@@ -2,7 +2,7 @@ import { Google } from 'arctic';
 import { env } from '$env/dynamic/private';
 
 export const google = new Google(
-	env.GOOGLE_CLIENT_ID,
-	env.GOOGLE_CLIENT_SECRET,
+	env.GOOGLE_CLIENT_ID!,
+	env.GOOGLE_CLIENT_SECRET!,
 	`${env.BASE_URL}/login/google/callback`
 );
